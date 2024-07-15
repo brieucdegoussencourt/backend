0d9a1cbec719dc9342e8f180f8d55c33c669e8a1
brieucdegoussencourt <brieuc.degoussencourt@gmail.com>
2024-07-15 13:53:09 +0200

    update

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const app = express();
 const mongoose = require('mongoose');
 console.log(mongoose.version);
 
+const Thing = require('./models/thing');
+
 
 mongoose.connect('mongodb+srv://brieucdegoussencourt:wiaoFjc6FGXVhegA@myclusterbrieuc.aprgjhv.mongodb.net/?retryWrites=true&w=majority&appName=MyClusterBrieuc')
   .then(() => {
@@ -28,10 +30,26 @@ app.use((req, res, next) => {
 });
 
 app.post('/api/stuff', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({
-        message: 'Thing created successfully!'
-    });
+  const thing = new Thing({
+    title: req.body.title,
+    description: req.body.description,
+    imageUrl: req.body.imageUrl,
+    price: req.body.price,
+    userId: req.body.userId
+  });
+  thing.save().then(
+    () => {
+      res.status(201).json({
+        message: 'Post saved successfully!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
 });
 
 app.get('/api/stuff', (req, res, next) => {
